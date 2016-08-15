ee3bdb82766d67eb06da267c6c2a2b82916c514b
Nanobot234 <Nbonsu8934@Taps391.org>
2016-08-15 16:23:33 -0400

    Salvico Inc app code (Classified) lol

---
+++ Test/app/src/main/java/com/example/olaoluwaolejede/test/BarSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.example.olaoluwaolejede.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BarSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_selection);
+    }
+}
